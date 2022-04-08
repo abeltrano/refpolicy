5ff89cc177ba3217d876a1f90de9a0ce6b1ad01f
Andrew Beltrano <anbeltra@microsoft.com>
2022-04-08 13:28:05 -0600

    Add initial packaging files.

---
+++ policy/wsl/include/WsaSelinuxPolicy.h
@@ -0,0 +1,7 @@
+#ifndef __WSA_SELINIX_POLICY_H__
+#define __WSA_SELINIX_POLICY_H__
+
+#define WSA_SELINUX_LABEL_INIT_STAGE1 "u:object_r:wsl_init_t:s0"
+#define WSA_SELINUX_LABEL_INIT_STAGE2 "u:r:kernel:s0"
+
+#endif // __WSA_SELINIX_POLICY_H__
