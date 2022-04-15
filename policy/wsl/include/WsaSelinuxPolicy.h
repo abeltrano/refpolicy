b4eccbf42a576233abd355c190a23d59548a460c
Andrew Beltrano <anbeltra@microsoft.com>
2022-04-15 10:52:06 -0600

    Update public header.

--- policy/wsl/include/WsaSelinuxPolicy.h
+++ policy/wsl/include/WsaSelinuxPolicy.h
@@ -1,7 +1,11 @@
-#ifndef __WSA_SELINIX_POLICY_H__
-#define __WSA_SELINIX_POLICY_H__
+//
+// Copyright (c) Microsoft Corporation.  All rights reserved.
+//
 
-#define WSA_SELINUX_LABEL_INIT_STAGE1 "u:object_r:wsl_init_t:s0"
-#define WSA_SELINUX_LABEL_INIT_STAGE2 "u:r:kernel:s0"
+#ifndef __WSA_SELINUX_POLICY_H__
+#define __WSA_SELINUX_POLICY_H__
 
-#endif // __WSA_SELINIX_POLICY_H__
+#define WSA_SELINUX_CONTEXT_WSL_INIT L"u:object_r:wsl_init_t:s0"
+#define WSA_SELINUX_CONTEXT_WSA_INIT L"u:r:kernel:s0"
+
+#endif // __WSA_SELINUX_POLICY_H__
